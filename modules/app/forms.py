0487bb013ad6860599bbfd0109a3d92e54d54571
Dominic Roberge <droberge@d-modules.com>
2013-08-08 11:00:25 -0400

    Infolettre et correctifs

--- modules/app/forms.py
+++ modules/app/forms.py
@@ -2,9 +2,40 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
 from crispy_forms.helper import FormHelper
+from django.core.validators import validate_email
 from crispy_forms.layout import Layout, Div, Submit, HTML, Button, Row, Field
 from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
- 
+
+class EmailForm(forms.Form):
+    nom = forms.CharField(
+        label = _("Votre nom:"),
+        max_length = 255,
+        required = True,
+    )
+
+    courriel = forms.CharField(
+        label = _("Votre courriel:"),
+        max_length = 255,
+        required = True,
+    )
+    
+    def __init__(self, *args, **kwargs):
+        super(EmailForm, self).__init__(*args, **kwargs)
+        
+        self.helper = FormHelper()
+        self.helper.form_id = 'emailSubscriptionForm'
+        self.helper.form_class = 'grid-100'
+        self.helper.form_method = 'post'
+        self.helper.form_action = '#emailSubscription_open'
+
+        self.helper.add_input(Submit(_('Envoyer'), _('Envoyer'),css_class='btn-orange btn-large'))
+        
+    def clean_courriel(self):
+        data = self.cleaned_data['courriel']
+        try :
+            validate_email(data)
+        except:
+            raise forms.ValidationError("Courriel invalide !")
  
 class ContactForm(forms.Form):
     message = ""
@@ -43,3 +74,10 @@ class ContactForm(forms.Form):
         self.helper.form_action = '#contactForm'
 
         self.helper.add_input(Submit(_('Envoyer'), _('Envoyer'),css_class='btn-orange btn-large'))
+
+    def clean_courriel(self):
+        data = self.cleaned_data['courriel']
+        try :
+            validate_email(data)
+        except:
+            raise forms.ValidationError("Courriel invalide !")
--- modules/app/urls.py
+++ modules/app/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('app.views',
     url(r'^$', 'home', name="home"),
+    url(r'^infolettre/desinscription/(?P<guid>.+)/$', 'unsubscription', name="unsubscription"),
     url(r'^contact/$', 'contact', name="contact"),
     url(r'^recherche/', include('haystack.urls')),
 )
